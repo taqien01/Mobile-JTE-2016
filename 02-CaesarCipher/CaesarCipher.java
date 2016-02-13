aaf5eae7125b008b85b210a670893a8072ac9010
Reza Muttaqien <taqien01@gmail.com>
2016-02-13 22:30:33 +0700

    no message

---
+++ 02-CaesarCipher/CaesarCipher.java
@@ -0,0 +1,95 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+/**
+ * Write a description of class CaesarCipher here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CaesarCipher
+{
+    
+    private int shift;
+    
+    public CaesarCipher(int shift){
+        this.shift=shift;
+
+    }
+    
+    public void enkripsi(String sumber, String sasaran) throws IOException{
+        /* Contoh dari file tutorial kopiBerkas.java dengan penambahan 
+         * seperlunya untuk mendapat keluaran seperti perintah
+           */
+        // Deklarasi variabel
+        FileInputStream masukan = null;
+        FileOutputStream keluaran = null;
+        
+        try {
+            // Object stream untuk masukkan
+            masukan = new FileInputStream(sumber);
+            // Object stream untuk masukkan
+            keluaran = new FileOutputStream(sasaran);
+            
+            // Coba baca  dari stream
+            int karakter = masukan.read();
+            long lokasi = 0;
+            // Selama masih ada data yang bisa dibaca
+            while (karakter != -1) {
+                
+                // Kirim ke keluaran
+                karakter += shift;
+                keluaran.write(karakter);
+                // Coba baca lagi dari stream
+                karakter = masukan.read();
+                ++lokasi;
+            }
+        } 
+        finally {
+            // Tutup stream masukan
+            if (masukan != null)
+                masukan.close();
+            // Tutup stream keluaran
+            if (keluaran != null)
+                keluaran.close();
+            }
+    }
+    
+    public void dekripsi(String sumber, String sasaran) throws IOException {
+        // Deklarasi variabel
+        FileInputStream masukan = null;
+        FileOutputStream keluaran = null;
+        
+        try {
+            // Object stream untuk masukkan
+            masukan = new FileInputStream(sumber);
+            // Object stream untuk masukkan
+            keluaran = new FileOutputStream(sasaran);
+            
+            // Coba baca  dari stream
+            int karakter = masukan.read();
+            long lokasi = 0;
+            // Selama masih ada data yang bisa dibaca
+            while (karakter != -1) {
+                
+                // Kirim ke keluaran
+                karakter -= shift;
+                keluaran.write(karakter);
+                // Coba baca lagi dari stream
+                karakter = masukan.read();
+                ++lokasi;
+            }
+        } 
+        finally {
+            // Tutup stream masukan
+            if (masukan != null)
+                masukan.close();
+            // Tutup stream keluaran
+            if (keluaran != null)
+                keluaran.close();
+            }
+    }
+    
+}
