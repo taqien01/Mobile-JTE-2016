c056fd58c4f22e2dcd6a9881db308ef42dfcf36d
Reza Muttaqien <taqien01@gmail.com>
2016-02-24 00:34:16 +0700

    Tugas03-Terjemahkan

---
+++ Tugas03-Terjemahkan/app/src/main/java/id/ac/unsyiah/elektro/mobile/tugas03_terjemahkan/MainActivity.java
@@ -0,0 +1,71 @@
+package id.ac.unsyiah.elektro.mobile.tugas03_terjemahkan;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickChanged (View view){
+        TextView txtTulisan = (TextView) findViewById(R.id.textView);
+        switch (view.getId()){
+            case R.id.tombolInd:
+                String aloDunia = getString(R.string.ind);
+                txtTulisan.setText(aloDunia);
+                break;
+            case R.id.tombolAceh:
+                String aloDunia2 = getString(R.string.aceh);
+                txtTulisan.setText(aloDunia2);
+                break;
+            case R.id.tombolIng:
+                String aloDunia3 = getString(R.string.ing);
+                txtTulisan.setText(aloDunia3);
+                break;
+        }
+    }
+}
