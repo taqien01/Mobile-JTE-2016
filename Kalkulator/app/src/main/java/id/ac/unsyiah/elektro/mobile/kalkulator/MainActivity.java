bde36fb913df58ea89824c6791c82e91a128f794
Reza Muttaqien <taqien01@gmail.com>
2016-02-27 15:40:42 +0700

    Praktikum Kalkulator

---
+++ Kalkulator/app/src/main/java/id/ac/unsyiah/elektro/mobile/kalkulator/MainActivity.java
@@ -0,0 +1,70 @@
+package id.ac.unsyiah.elektro.mobile.kalkulator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickSamaDengan(View view){
+        EditText numAngka1 = (EditText) findViewById(R.id.numAngka1);
+        String angkaStr = numAngka1.getText().toString();
+        long angka1 = Long.valueOf(angkaStr);
+
+        EditText numAngka2 = (EditText) findViewById(R.id.numAngka2);
+        String angkaStr2 = numAngka2.getText().toString();
+        long angka2 = Long.valueOf(angkaStr2);
+
+        long hasil = angka1 + angka2;
+        String hasilStr = Long.toString(hasil);
+
+        TextView hasilTampil = (TextView) findViewById(R.id.hasil);
+        hasilTampil.setText(hasilStr);
+    }
+}
