f79416606415218b749c3f40c239772ff14fea67
Reza Muttaqien <taqien01@gmail.com>
2016-03-11 17:41:47 +0700

    Tugas 06 Joystick

---
+++ Tugas06-Joystick/app/src/main/java/id/ac/unsyiah/elektro/mobile/tugas06_joystick/TombolFrag.java
@@ -0,0 +1,22 @@
+package id.ac.unsyiah.elektro.mobile.tugas06_joystick;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TombolFrag extends Fragment {
+
+    public TombolFrag() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tombol, container, false);
+    }
+}
